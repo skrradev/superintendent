ddd7d7bf16a12cab4e97f3780fd733d735a16093
skrradev <skrra.dev@gmail.com>
2019-02-14 17:01:59 +0600

    ibnitial

---
+++ src/main/java/kz/zhan/superintendent/SuperintendentApplication.java
@@ -0,0 +1,14 @@
+package kz.zhan.superintendent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperintendentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperintendentApplication.class, args);
+    }
+
+}
+
